00641e1dca9de7896c50ea7e700aad212c8b912a
Tori Enyart <torienyart@gmail.com>
2022-10-15 21:44:07 -0600

    Add some silly puts statements

--- variables.rb
+++ variables.rb
@@ -13,3 +13,13 @@ amount_paid = 1200.00
  
 deposit_paid = true
 computer_recieved = true
+
+
+f_name = "not Victoria"
+l_name = "not Enyart"
+preferred_name = "I prefer to go by Tori"
+
+
+puts "I am writing some puts statements, to begin, I'll introduce myself..... I'm #{f_name}"
+
+puts "Actualy, usually #{preferred_name}"
